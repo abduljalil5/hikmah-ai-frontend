e576d16c28aebcd3ef15c3d1c51815ec5038cb2b
Abdul Jalil <aj072205@gmail.com>
2025-04-16 22:56:21 +0500

    Update FileUpload.jsx

--- hackma-ai-front-end/src/components/FileUpload.jsx
+++ hackma-ai-front-end/src/components/FileUpload.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+// import React from 'react'
 
-function FileUpload() {
-  return (
-    <div>FileUpload</div>
-  )
-}
+// function FileUpload() {
+//   return (
+//     <div>FileUpload</div>
+//   )
+// }
 
-export default FileUpload
+// export default FileUpload
+
+
+const handleFileUpload = async (file) => {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  try {
+    const response = await fetch("https://fancy-regal-peony.glitch.me/api/upload", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+    console.log("Upload Success:", data.message);
+  } catch (err) {
+    console.error("Upload Failed:", err);
+  }
+};
